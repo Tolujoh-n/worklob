1c499f9e915a2f1650000e7083488b6282f0463a
Tolu john <104234518+Tolujoh-n@users.noreply.github.com>
2025-02-14 03:51:21 +0100

    check profile

--- src/components/profile/Editprofile.jsx
+++ src/components/profile/Editprofile.jsx
@@ -3,8 +3,25 @@ import Personalinfo from "./Personalinfo";
 import Workexperience from "./Workexperience";
 import Education from "./Education";
 import Sociallink from "./Sociallink";
+import { jwtDecode } from "jwt-decode";
 
 const EditProfile = () => {
+  const token = localStorage.getItem("token");
+
+  let userId;
+
+  const user = JSON.parse(localStorage.getItem("user"));
+  let userRole = user?.role;
+  console.log("User Role:", userRole);
+  console.log("User ID:", user);
+
+  if (token) {
+    const decodedToken = jwtDecode(token);
+    userId = decodedToken.userId;
+  }
+
+  const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
+
   return (
     <div className="gigprofile container">
       {/* Personal Information Section */}
